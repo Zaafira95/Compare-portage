3fc1951c76d0bb826338058320f37d2a6fe9170b
Zaafira95 <104431117+Zaafira95@users.noreply.github.com>
2022-09-14 23:00:59 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-ijiji
+
+  var url_string = window.location.href;
+  var url = new URL(url_string);
+  const formatter1 = new Intl.NumberFormat('fr-FR', {
+  style: 'currency',currency: 'EUR',minimumFractionDigits: 2
+})
+const formatter2 = new Intl.NumberFormat('fr-FR', {
+  style: 'currency', currency: 'EUR',minimumFractionDigits: 0
+})
+
+  
