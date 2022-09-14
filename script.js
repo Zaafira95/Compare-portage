1141a88667244b829c0b02951dd00007ed8a1fac
Zaafira95 <104431117+Zaafira95@users.noreply.github.com>
2022-09-14 23:26:00 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+  var url_string = window.location.href;
   var url = new URL(url_string);
   const formatter1 = new Intl.NumberFormat('fr-FR', {
   style: 'currency',currency: 'EUR',minimumFractionDigits: 2
